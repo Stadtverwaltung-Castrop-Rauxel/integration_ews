dea4aadcf4c6355b5cfad0672cb5e20e4834006a
InfotechnikCR <126780164+InfotechnikCR@users.noreply.github.com>
2026-09-14 09:51:02 +0200

    Update de.js
    added translation

--- l10n/de.js
+++ l10n/de.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "integration_ews",
       {
+    "--- Do not synchronize ---": "---Nicht synchronisieren ---",
     "Connected accounts": "E-Mailkonto (Exchange)",
     "Failed to retrieve preferences": "Einstellungen konnten nicht abgerufen werden",
     "Failed to authenticate with EWS server": "Authentifizierung beim EWS-Server fehlgeschlagen",
